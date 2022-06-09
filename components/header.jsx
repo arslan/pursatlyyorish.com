ac698ca573f914153bcc7ff9eba0398ec337ad8d
jaha-sudo <romanovajaha@gmail.com>
2022-06-09 14:55:19 +0500

    install next-i18nex for translation, add common.json

--- components/header.jsx
+++ components/header.jsx
@@ -1,34 +1,43 @@
+import { useTranslation } from "next-i18next";
 import React, { useState } from "react";
 import styles from "../styles/Home.module.css";
-const Header = ()=>{
+import { MdKeyboardArrowDown } from "react-icons/md";
+import Image from "next/image";
 
-    const [lang,setLang] = useState("Русский");
-    const [langOpen,setLangOpen] = useState(false)
+const Header = () => {
+    const { t } = useTranslation("common")
 
-    const changeLang = (lang)=>{
+
+    const [lang, setLang] = useState("Русский");
+    const [langOpen, setLangOpen] = useState(false)
+
+    const changeLang = (lang) => {
         setLangOpen(!langOpen);
         setLang(lang)
     }
-    return(
-        <div  className={styles.header}>
+    return (
+        <div className={styles.header}>
             <div>
                 <h1>Pursatly Ýöriş</h1>
 
             </div>
             <div>
-                <h3>О нас</h3>
-                <h3>Услуги</h3>
-                <h3>Контакты</h3>
+                <h3>{t("common.aboutUS")}</h3>
+                <h3>{t("common.services")}</h3>
+                <h3>{t("common.contacts")}</h3>
                 <div className={styles.lang}>
-                    <h3 onClick={()=>setLangOpen(!langOpen)}>{lang}</h3>
-                   {langOpen && <div className={styles.langs}>
-                        <span className={styles.ActiveLang} onClick={()=>changeLang("Русский")}>Русский</span>
-                        <span onClick={()=>changeLang("English")}>English</span>
-                        <span onClick={()=>changeLang("Turkmen")}>Turkmen</span>
+                    <h3 onClick={() => setLangOpen(!langOpen)}>{lang}
+                        <MdKeyboardArrowDown />
+
+                    </h3>
+                    {langOpen && <div className={styles.langs}>
+                        <span className={styles.ActiveLang} onClick={() => changeLang("Русский")}>Русский</span>
+                        <span onClick={() => changeLang("English")}>English</span>
+                        <span onClick={() => changeLang("Turkmen")}>Turkmen</span>
                     </div>}
                 </div>
                 <div className={styles.verticalLine}></div>
-                <h3><img src="./Icon.svg"/>+993 65039539</h3>
+                <h3><Image src="/Icon.svg" width={20} height={20} alt="" />&nbsp;+993 65039539</h3>
             </div>
         </div>
     )
