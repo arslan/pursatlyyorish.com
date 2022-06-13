e3f392c9f450e1874f61a5f20c2d763ac8314d21
jaha-sudo <romanovajaha@gmail.com>
2022-06-13 14:48:34 +0500

    adaptive footer, form

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,6 +5,25 @@ module.exports = {
   ],
   theme: {
     extend: {},
+    screens: {
+      'sm': '320px',
+      // => @media (min-width: 320px) { ... }
+
+      'md': '640px',
+      // => @media (min-width: 640px) { ... }
+
+      'lg': '768px',
+      // => @media (min-width: 768px) { ... }
+      'xl': '1024px',
+      // => @media (min-width: 1024px) { ... }
+
+      '2xl': '1280px',
+      // => @media (min-width: 1280px) { ... }
+
+      '3xl': '1536px',
+      // => @media (min-width: 1536px) { ... }
+    },
   },
+
   plugins: [],
 }
