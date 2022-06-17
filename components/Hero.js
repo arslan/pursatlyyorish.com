ad12fb5aba3bd8c2b8ba106bd364d72cf759d5cc
Arslan A <arslan@hillitilsimat.com>
2022-06-17 11:33:23 +0500

    fix hero image

--- components/Hero.js
+++ components/Hero.js
@@ -3,7 +3,7 @@ import { useTranslation } from "next-i18next";
 function Hero() {
     const { t } = useTranslation("common");
     return (
-        <div className="bg-[url('/background-hero.webp')] bg-fixed bg-cover h-[calc(60vh)] flex items-center">
+        <div className="bg-[url('/background-hero.webp')] bg-fixed h-[calc(60vh)] flex items-center">
             <div className="flex flex-col items-center my-12 max-w-7xl mx-auto">
                 <h1 className="mb-4 text-4xl font-black text-center text-white transition-colors duration-200 ease-in-out w-fit hover:text-green">
                     PURSATLY ÝÖRIŞ
